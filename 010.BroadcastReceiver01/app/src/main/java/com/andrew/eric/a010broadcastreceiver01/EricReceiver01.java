4be6d7d7e75a43913406d08448e5a414c5a71370
uheeyc99 <genghis.bupt@gmail.com>
2017-05-15 22:06:11 +0800

    010

---
+++ 010.BroadcastReceiver01/app/src/main/java/com/andrew/eric/a010broadcastreceiver01/EricReceiver01.java
@@ -0,0 +1,10 @@
+package com.andrew.eric.a010broadcastreceiver01;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by eric on 2017/5/15.
+ */
+
+public class EricReceiver01 extends BroadcastReceiver {
+}
