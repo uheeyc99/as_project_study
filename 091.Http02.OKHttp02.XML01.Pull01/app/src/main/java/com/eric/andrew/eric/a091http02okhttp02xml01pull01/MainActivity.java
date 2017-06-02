b4f0620f74d5b8f89d6ae1c17606beb4a6411021
Eric Gao <uheeyc@126.com>
2017-06-03 02:24:46 +0800

    json

--- 091.Http02.OKHttp02.XML01.Pull01/app/src/main/java/com/eric/andrew/eric/a091http02okhttp02xml01pull01/MainActivity.java
+++ 091.Http02.OKHttp02.XML01.Pull01/app/src/main/java/com/eric/andrew/eric/a091http02okhttp02xml01pull01/MainActivity.java
@@ -1,13 +1,169 @@
 package com.eric.andrew.eric.a091http02okhttp02xml01pull01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+    TextView tv;
+    String urlString = "http://192.168.56.101/g.xml";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        btn = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.textView);
+        btn.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+//        sendRequestWithHttpURLConnection();
+        sendRequestWithOkHttp();
+    }
+
+    private void parseXMLWithPull(String xmlData){
+
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+
+                switch (eventType){
+                    case XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        }else
+                        if("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        }else
+                        if("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+
+                    }
+                        break;
+
+                    case XmlPullParser.END_TAG:{
+                        if("app".equals(nodeName)){
+                            Log.i("MainActivity","id is " + id);
+                            Log.i("MainActivity","name is " + name);
+                            Log.i("MainActivity","version is " + version);
+                        }
+                    }
+                        break;
+
+                    default:
+                        break;
+                }
+                eventType=xmlPullParser.next();
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+    private void sendRequestWithOkHttp() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                try {
+                    Request request = new Request.Builder().url(urlString).build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    showResponse(responseData);
+                    parseXMLWithPull(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t = new Thread(r);
+        t.start();
+    }
+
+    private void sendRequestWithHttpURLConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL(urlString);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while((line = reader.readLine())!=null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if(reader !=null){
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }
+        }).start();
+    }
+
+    private void showResponse(final String s) {
+        //Runs the specified action on the UI thread
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tv.setText(s);
+            }
+        });
     }
 }
--- 091.Http02.OKHttp03.JSON01.JSONObject01/app/src/main/java/com/eric/andrew/eric/a091http02okhttp03json01jsonobject01/MainActivity.java
+++ 091.Http02.OKHttp03.JSON01.JSONObject01/app/src/main/java/com/eric/andrew/eric/a091http02okhttp03json01jsonobject01/MainActivity.java
@@ -1,19 +1,16 @@
-package com.eric.andrew.eric.a091http02okhttp02xml02sax01;
+package com.eric.andrew.eric.a091http02okhttp03json01jsonobject01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.xml.sax.Attributes;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.StringReader;
 
@@ -26,7 +23,7 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button btn;
     TextView tv;
-    String urlString = "http://192.168.56.101/get_data.xml";
+    String urlString = "http://192.168.56.101/get_data.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
                     showResponse(responseData);
-                    parseXMLWithSAX(responseData);
+                    parseJSONObject(responseData);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -64,13 +61,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         t.start();
     }
 
-    private void parseXMLWithSAX(String xmlData) {
+    private void parseJSONObject(String jsonData) {
         try {
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            XMLReader xmlReader = factory.newSAXParser().getXMLReader();
-            MyHandler01 handler = new MyHandler01();
-            xmlReader.setContentHandler(handler);
-            xmlReader.parse(new InputSource(new StringReader(xmlData)));
+            JSONArray jsonArry = new JSONArray(jsonData);
+            for(int i=0;i<jsonArry.length();i++){
+                JSONObject jsonObject = jsonArry.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String version = jsonObject.getString("version");
+                String name = jsonObject.getString("name");
+                Log.i("MainActivity","id is " + id);
+                Log.i("MainActivity","name is " + name);
+                Log.i("MainActivity","version is " + version);
+
+            }
+
         }catch (Exception e){
             e.printStackTrace();
         }
