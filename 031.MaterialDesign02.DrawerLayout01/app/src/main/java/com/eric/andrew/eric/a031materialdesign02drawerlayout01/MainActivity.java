8e7eadacb7ad33fe5193c6676926a5eac99dc8fc
Eric Gao <uheeyc@126.com>
2017-06-06 15:05:00 +0800

    s

--- 031.MaterialDesign02.DrawerLayout01/app/src/main/java/com/eric/andrew/eric/a031materialdesign02drawerlayout01/MainActivity.java
+++ 031.MaterialDesign02.DrawerLayout01/app/src/main/java/com/eric/andrew/eric/a031materialdesign02drawerlayout01/MainActivity.java
@@ -1,13 +1,53 @@
 package com.eric.andrew.eric.a031materialdesign02drawerlayout01;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBar actionBar;
+    Toolbar toolBar;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        actionBar = getSupportActionBar();
+
+
+        if(actionBar != null){
+            Toast.makeText(this, "aaab", Toast.LENGTH_SHORT).show();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+
+        }
+
+
+
     }
 }
