b60139e060e0e5af54c9753b6ac0022aa8799341
Eric Gao <genghis.bupt@gmail.com>
2017-07-24 15:27:25 +0800

    101.UMeng01.push01

---
+++ 101.UMeng01.push01/app/src/main/java/com/example/eric/a101umeng01push01/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eric.a101umeng01push01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
